fa6bfe4e0c15ea2efeb43c48f1083048d9dc5faf
frega <fl@flink-solutions.de>
2019-05-07 12:57:32 +0200

    Rebuild to reflect changes of 'the-great-clean-up'

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,11 +1,14 @@
 // The editor creator to use.
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Link from "@ckeditor/ckeditor5-link/src/link";
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import List from '@ckeditor/ckeditor5-list/src/list';
@@ -13,44 +16,38 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Template from '@amazee/ckeditor5-template/src/template';
-import Linkit from '@amazee/ckeditor5-drupal-linkit/src/linkit';
-import DrupalMedia from '@amazee/ckeditor5-drupal-media/src/drupalmedia';
 import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
-import Validation from '@amazee/ckeditor5-template/src/validation';
-import ButtonElement from '@amazee/ckeditor5-drupal-linkit/src/elements/buttonelement';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+
 import TemplateEditing from '@amazee/ckeditor5-template/src/templateediting';
 import RemoteControl from '@amazee/ckeditor5-template/src/remotecontrol';
-import TabsElement from '@amazee/ckeditor5-template/src/elements/tabselement';
-import MergeEditing from '@amazee/ckeditor5-template/src/mergeediting';
+import MergeEditing from "@amazee/ckeditor5-template/src/mergeediting";
 
-import Placeholder from '@amazee/editor-components/components/placeholder/placeholder';
+import "@amazee/editor-components/components/base/placeholder/index";
+import "@amazee/editor-components/components/container/index";
+import "@amazee/editor-components/components/section/index";
+import "@amazee/editor-components/components/gallery/index";
+import "@amazee/editor-components/components/tabs/index";
+import "@amazee/editor-components/components/button/index";
+import "@amazee/editor-components/components/text_conflict/index";
+import "@amazee/editor-components/components/media_conflict/index";
+import "@amazee/editor-components/components/media/index";
+import "@amazee/editor-components/components/textfield/index";
 
-import Media from '@amazee/editor-components/components/media/media';
-import '@amazee/editor-components/components/container/container';
-import '@amazee/editor-components/components/gallery/gallery';
-import '@amazee/editor-components/components/tabs/tabs';
-import '@amazee/editor-components/components/text_conflict/text_conflict';
-import '@amazee/editor-components/components/text_conflict/text_conflict_option/text_conflict_option';
 
-export default class SectionsEditor extends BalloonEditorBase {}
-
-class PlaceholderConfig extends Plugin {
-	init() {
-		const templates = this.editor.config.get( 'templates' );
-		Placeholder.availableSections = Object.keys( templates )
-			.map( id => ( { id, label: templates[ id ].label, icon: templates[ id ].icon } ) );
-		Media.previewCallback = this.editor.config.get( 'drupalMediaRenderer' ).callback;
-	}
-}
+export default class SectionsEditor extends BalloonEditorBase { }
 
 // Plugins to include in the build.
 SectionsEditor.builtinPlugins = [
-	PlaceholderConfig,
 	RemoteControl,
 	Essentials,
+	PasteFromOffice,
 	Autoformat,
 	Bold,
 	Italic,
+	Subscript,
+	Superscript,
+	Underline,
 	BlockQuote,
 	Heading,
 	List,
@@ -59,136 +56,21 @@ SectionsEditor.builtinPlugins = [
 	TableToolbar,
 	BlockToolbar,
 	Template,
-	Linkit,
 	TemplateEditing,
-	DrupalMedia,
-	Validation,
-	TabsElement,
-	ButtonElement,
-	MergeEditing
+	MergeEditing,
+	Link,
 ];
 
 // Editor configuration.
 SectionsEditor.defaultConfig = {
-	templates: {
-		root: {
-			label: 'Root',
-			template: '<div class="root" ck-type="container" ck-contains="text paragraph text_media gallery tabs columns"></div>',
-		},
-		paragraph: {
-			label: 'Paragraph',
-			icon: 'text',
-			template:
-				'<div class="paragraph__wrapper"><p class="paragraph" ck-type="text"></p></div>',
-		},
-		text: {
-			label: 'Text',
-			icon: 'text',
-			template:
-				'<div class="text" id="">' +
-					'<h2 class="text__headline" ck-type="text" ck-plain="true">Insert a headline</h2>' +
-					'<div class="text__content" ck-type="text"></div>' +
-				'</div>',
-		},
-		text_media: {
-			label: 'Text & Media',
-			icon: 'text_media',
-			template:
-				'<div class="text-media" id="" data-layout="right">' +
-					'<h2 class="text-media__headline" ck-type="text" ck-plain="true">Insert a headline</h2>' +
-					'<div class="text-media__wrapper">' +
-						'<div class="text-media__content" ck-type="text"></div>' +
-						'<div class="text-media__media" ck-type="placeholder" ck-conversions="image"></div>' +
-					'</div>' +
-					'<div class="text-media__cta" ck-type="button">Call to action!</div>' +
-				'</div>',
-		},
-		image: {
-			label: 'Image',
-			icon: 'image',
-			template: '<div class="media-wrapper">' +
-				'<div class="text-media__media" ck-type="drupal-media" data-media-type="image" data-media-uuid=""></div>' +
-				'</div>',
-		},
-		video: {
-			label: 'Image',
-			icon: 'video',
-			template: '<div class="media-wrapper">' +
-				'<div class="text-media__media" ck-type="drupal-media" data-media-type="image" data-media-uuid=""></div>' +
-				'</div>',
-		},
-		media: {
-			label: 'Media',
-			icon: 'misc',
-			template: '<div class="media-item" ck-type="placeholder" ck-conversions="image video"></div>',
-		},
-		gallery: {
-			label: 'Gallery',
-			icon: 'carousel',
-			template:
-				'<div class="gallery_wrapper"><div class="gallery" ck-type="gallery" ck-contains="media"></div></div>',
-		},
-		columns: {
-			label: 'Columns',
-			icon: 'misc',
-			template:
-			'<div class="two-columns"><div class="two-col-wrapper">' +
-				'<div class="column-a" ck-type="container" ck-contains="text media gallery"></div>' +
-				'<div class="column-b" ck-type="container" ck-contains="text media gallery"></div>' +
-			'</div></div>'
-		},
-		tabs: {
-			label: 'Tabs',
-			icon: 'misc',
-			template:
-				'<div class="tabs_wrapper"><div class="tabs" ck-type="tabs" ck-contains="tab"></div></div>',
-		},
-		tab: {
-			label: 'Tab',
-			template:
-				'<div class="tab_wrapper" data-tab-title="" data-default-tab=""><div class="tab" ck-type="gallery" ck-contains="image"></div></div>',
-		}
-	},
-	masterTemplate: 'root',
-	templateAttributes: {
-		'data-tab-title': {
-			type: 'custom',
-		},
-		'data-default-tab': {
-			type: 'custom',
-		},
-		'id': {
-			type: 'textfield',
-			label: 'HTML ID',
-		},
-		'data-layout': {
-			type: 'dropdown',
-			label: 'Layout',
-			options: {
-				left: 'Media on the left',
-				right: 'Media on the right',
-			}
-		}
-	},
-	drupalMediaSelector: { callback: ( type, operation, callback ) => {
-		callback( operation === 'add' ? '300' : '400' );
-	} },
-	drupalEntitySelector: { callback: ( type, operation, callback ) => {
-		callback( operation === 'add' ? '100' : '200' );
-	} },
-	drupalMediaRenderer: { callback: ( uuid, display, callback ) => {
-		window.setTimeout( () => {
-			callback( `<img src="https://picsum.photos/800/${ uuid }"/>` );
-		}, 2000 );
-	} },
-	drupalLinkSelector: { callback: ( existingValues, callback ) => {
-		callback( { href: 'http://www.drupal.org' } );
-	} },
 	toolbar: {
 		items: [
 			'bold',
 			'italic',
 			'link',
+			'underline',
+			'superscript',
+			'subscript',
 			'bulletedList',
 			'numberedList',
 			'blockQuote',
@@ -204,7 +86,7 @@ SectionsEditor.defaultConfig = {
 			'mergeTableCells'
 		]
 	},
-	blockToolbar: [ 'heading', 'insertTable' ],
+	blockToolbar: ['heading', 'insertTable'],
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
--- tests/manual/ckeditor.js
+++ tests/manual/ckeditor.js
@@ -1,14 +1,82 @@
-/**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
- */
-
 import SectionsEditor from '../../build/ckeditor';
 
-SectionsEditor.create( document.querySelector( '#editor' ) )
-	.then( editor => {
-		window.editor = editor;
-	} )
-	.catch( err => {
-		console.error( err.stack );
-	} );
+const options = {
+	masterTemplate: 'page',
+	templates: {
+		"page": {
+			"label": "Page",
+			"icon": "text",
+			"template": "<div class=\"page\" itemtype=\"page\">\n  <ck-container class=\"page__container\" itemprop=\"sections\" ck-contains=\"text image teaser\"></ck-container>\n</div>\n"
+		},
+		"teaser": {
+			"label": "Teaser",
+			"icon": "text",
+			"template": "<ck-section class=\"teaser\" itemtype=\"teaser\" data-layout=\"\">\n  <div data-media-type=\"media:image\"\n       data-media-uuid=\"\"\n       itemprop=\"image\"\n       itemtype=\"teaser-image\"\n       class=\"teaser__image\"\n  ></div>\n  <div class=\"teaser__content\">\n    <h2 ck-input=\"plain\" itemprop=\"headline\" class=\"teaser__headline\">Headline</h2>\n    <div ck-input=\"full\" itemprop=\"text\" class=\"teaser__text\">Content</div>\n    <ck-button itemtype=\"button\" itemprop=\"link\" class=\"teaser__link\" link-target=\"\">\n      <div itemprop=\"text\" ck-input=\"plain\">Link text placeholder</div>\n    </ck-button>\n  </div>\n</ck-section>\n"
+		},
+		"image": {
+			"label": "Image",
+			"icon": "image",
+			"template": "<ck-section class=\"image\" itemtype=\"image\">\n  <ck-media\n    data-media-type=\"media:image\"\n    data-media-uuid=\"\"\n    itemprop=\"content\"\n    itemtype=\"media\"\n    class=\"image__media\"\n  ></ck-media>\n  <div class=\"image__caption\" ck-input=\"basic\" itemprop=\"caption\">Caption</div>\n</ck-section>\n"
+		},
+		"text": {
+			"label": "Text",
+			"icon": "text",
+			"template": "<div class=\"text\" itemtype=\"text\">\n    <h2 ck-input=\"plain\" itemprop=\"headline\">Enter a headline ...</h2>\n    <div class=\"text__content\" ck-input=\"full\" itemprop=\"content\">Enter some content ...</div>\n</div>\n"
+		}
+	},
+	templateAttributes: null,
+	templateSession: 'random'
+};
+
+const editor = document.querySelector( '#editor' );
+initEditor(editor, options);
+
+function initEditor(editor, options) {
+	editor.classList.add('ck-editor');
+
+	editor.addEventListener('ck-editor:available-sections', function (event) {
+		var sections = Object.keys(options.templates).map(id => ({
+			id: id,
+			label: options.templates[id].label,
+			icon: options.templates[id].icon
+		}));
+		event.respond(sections)
+	});
+
+	function handleSelect(event, response) {
+		setTimeout(function() {
+			event.respond(response);
+		}, 500);
+	}
+
+	editor.addEventListener('ck-editor:media-select', function (event) {
+		handleSelect(event, '400');
+	});
+
+	editor.addEventListener('ck-editor:media-upload', function (event) {
+		handleSelect(event, '500');
+	});
+
+	editor.addEventListener('ck-editor:media-edit', function (event) {
+		handleSelect(event, '600');
+	});
+
+	editor.addEventListener('ck-editor:media-preview', function (event) {
+		// @note:
+		const uuid = event.detail.uuid;
+		const height = parseInt(uuid, 10);
+		return event.respond('<img src="https://picsum.photos/600/' + height + '" />');
+	});
+
+	editor.addEventListener('ck-editor:select-link', function (event) {
+		event.respond('https://example.com');
+	});
+
+	SectionsEditor.create( editor, options )
+		.then( editor => {
+			window.editor = editor;
+		} )
+		.catch( err => {
+			console.error( err.stack );
+		} );
+}
